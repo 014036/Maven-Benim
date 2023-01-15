db4dc73ac48bd8f18d79c3e7c0053d6d5f80e4a7
014036 <kusulu78@gmail.com>
2023-01-15 21:38:38 +0300

    15OCak2023-7

---
+++ src/test/java/alistirmalar/slayt_sorulari/cozmedim/Sayfa_144_WebTable.java
@@ -0,0 +1,58 @@
+package alistirmalar.slayt_sorulari.cozmedim;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+public class Sayfa_144_WebTable extends TestBase {
+
+    /*
+
+1- https://www.hotelmycamp.com admin/HotelRoomAdmin adresine gidin
+
+ 2- Ana Sayfadaki login butonuna tiklayin
+ 3-Log in butonuna basiniz
+ 4-acilan ekrana asagidaki bilgileri giriniz
+Username : manager
+ Password : Manager1!
+2- table( ) metodu oluşturun
+3-Tüm table body’sinin boyutunu(sutun sayisi) bulun.
+4-Table’daki tum body’I ve başlıkları(headers) konsolda yazdırın.
+5-printRows( ) metodu oluşturun
+6-//tr table body’sinde bulunan toplam satir(row) sayısını bulun.
+7-Table body’sinde bulunan satirlari(rows) konsolda yazdırın.
+8- 4.satirdaki(row) elementleri konsolda yazdırın.
+     */
+
+    @Test
+    public void login() {
+//1- https://www.hotelmycamp.com admin/HotelRoomAdmin adresine gidin
+        driver.get("https://www.hotelmycamp.com");
+
+ //2- Ana Sayfadaki login butonuna tiklayin
+        driver.findElement(By.xpath("//a[@href='/Account/Logon']")).click();
+        //acilan ekrana asagidaki bilgileri giriniz
+        driver.findElement(By.id("UserName")).sendKeys("manager", Keys.TAB,"Manager1!");
+//Username : manager
+// Password : Manager1!
+   //Log in butonuna basiniz
+   driver.findElement(By.id("btnSubmit")).click();
+
+//2- table( ) metodu oluşturun
+        table();
+//3-Tüm table body’sinin boyutunu(sutun sayisi) bulun.
+//4-Table’daki tum body’I ve başlıkları(headers) konsolda yazdırın.
+//5-printRows( ) metodu oluşturun
+//6-//tr table body’sinde bulunan toplam satir(row) sayısını bulun.
+//7-Table body’sinde bulunan satirlari(rows) konsolda yazdırın.
+//8- 4.satirdaki(row) elementleri konsolda yazdırın.
+
+    }
+
+    public void table(){
+  WebElement bodySize=      driver.findElement(By.xpath("//div[@class='portlet-body']"));
+        System.out.println("bodySize.getSize() = " + bodySize.getSize());
+    }
+}
---
+++ src/test/java/alistirmalar/slayt_sorulari/cozmedim/Sayfa_146_WebTable.java
@@ -0,0 +1,17 @@
+package alistirmalar.slayt_sorulari.cozmedim;
+
+import utilities.TestBase;
+
+public class Sayfa_146_WebTable extends TestBase {
+
+    /*
+    WebTables class’ini kullanin.
+1. Bir metod oluşturun : printData(int row, int column);
+a. Satır(row) ve sütun(column) numarasını girdiğinizde, printData metodu bu
+hücredeki(cell) veriyi yazdırmalıdır.
+2. Baska bir Test metodu oluşturun: printDataTest( );
+a. Ve bu metodu printData( ) methodunu cagirmak icin kullanin.
+b. Örnek: printData (3,5); => 3. satır, 5. Sütundaki veriyi yazdırmalıdır
+c. yazdirilan datanin olmasi gereken dataya esit oldugunu test edin
+     */
+}
