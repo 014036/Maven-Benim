79bbd2f87059f0b59449f09a6c8d70e129a22884
014036 <kusulu78@gmail.com>
2023-01-15 16:48:05 +0300

    15OCak2023-3

--- src/test/java/alistirmalar/cesitli_sorular/BankOfAmerikaWebTable.java
+++ src/test/java/alistirmalar/cesitli_sorular/BankOfAmerikaWebTable.java
@@ -4,20 +4,18 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import utilities.TestBase;
 
-public class WebTables extends TestBase {
+public class BankOfAmerikaWebTable extends TestBase {
 
 
     @Test
-    public void name() {
+    public void test(){
 
-        //    https://the-internet.herokuapp.com/tables
-        driver.get("https://the-internet.herokuapp.com/tables");
 
-        String allTables= driver.findElement(By.id("table1")).getText();
-        System.out.println("allTables = " + allTables);
+        driver.get("https://www.bankofamerica.com/");
         rowColumnDataPrint("1", "2", "3");
-
+//Are Not a Condition to Any Banking Service or Activity
     }
+
 
     public void rowColumnDataPrint(String tableNumber, String rowNumber, String columnNumber){
         String rowData=    driver.findElement(By.xpath("//table["+tableNumber+"]//tr["+rowNumber+"]//td["+columnNumber+"]")).getText();
