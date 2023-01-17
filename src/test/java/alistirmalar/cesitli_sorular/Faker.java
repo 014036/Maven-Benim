908a9c6fbcb23a266029cd2c66208dbd5217e090
014036 <kusulu78@gmail.com>
2023-01-17 22:12:19 +0300

    17OCak2023-2

---
+++ src/test/java/alistirmalar/cesitli_sorular/Faker.java
@@ -0,0 +1,25 @@
+package alistirmalar.cesitli_sorular;
+
+import utilities.TestBase;
+
+public class Faker extends TestBase {
+
+    /*
+                               - FAKER -
+        Faker class'ı, adlar, telefon numaraları, adresler, SSN vb.
+    gibi gerçek görünümlü verileri dinamik olarak oluşturmanıza olanak tanır.
+    Web uygulamanızın bunları doğru bir şekilde kaydedeceğini bildiğiniz sürece,
+    çoğu zaman ad veya adres gibi bu değerlerin ne olduğu umurunuzda olmaz ve faker kullanmak
+    bu verileri modellemenize ve bu alanlar için şimdiye kadar test çalıştırması için yeni dinamik
+    değerler oluşturmanıza olanak tanır.
+        Faker class'ını kullanabilmemiz için mvn repository adresinden java faker kütüphanesini
+    projemize eklememiz gerekmektedir.
+
+
+
+     //"https://demoqa.com/automation-practice-form"  Adresine gidin
+    // Formu doldurunuz
+    // Sayfayi kapatin
+     */
+
+}
